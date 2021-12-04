2bd9095b9a900f645db2a59dada19a8c7f7f87bd
fipelle <6272230+fipelle@users.noreply.github.com>
2021-12-04 02:33:01 +0000

    Updated beta accordingly

--- macro_selection.jl
+++ macro_selection.jl
@@ -121,7 +121,7 @@ selection_sample = data_vintages[1][!,2:end] |> JMatrix{Float64};
 selection_sample = permutedims(selection_sample);
 
 # Validation inputs: common for all err_types
-gamma_bounds = ([12, 12], [0.01, 2.50], [0.0, 1.0], [1.0, 2.0]);
+gamma_bounds = ([12, 12], [0.01, 2.50], [0.0, 1.0], [1.0, 1.2]);
 grid_prerun = HyperGrid(gamma_bounds..., 1);
 grid = HyperGrid(gamma_bounds..., 1000);
 weights = zeros(n_series);
