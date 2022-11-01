0f9165417d0a13954815f9191af17d9526a5e477
fipelle <6272230+fipelle@users.noreply.github.com>
2022-11-01 20:12:49 +0000

    add final breakpoints

--- src/finance_functions.jl
+++ src/finance_functions.jl
@@ -104,14 +104,18 @@ Update and return `existing_sspace`.
 function get_sspace(macro_data::Union{FloatMatrix, JMatrix{Float64}}, t0::Int64, optimal_hyperparams::FloatVector, model_args::Tuple, model_kwargs::NamedTuple, coordinates_params_rescaling::Vector{Vector{Int64}}, existing_sspace::Nothing=nothing, existing_std_diff_data::Nothing=nothing)
 
     # Get trend-cycle model structure (estimated with data up to t0 - included)
+    @infiltrate
     estim, std_diff_data = get_tc_structure(macro_data[:, 1:t0], optimal_hyperparams, model_args, model_kwargs, coordinates_params_rescaling);
-
+    @infiltrate
+    
     # Estimate the trend-cycle model with (estimated with data up to t0 - included)
     return ecm(estim, output_sspace_data=macro_data./std_diff_data); # using the optional keyword argument `output_sspace_data` allows to construct the validation samples
 end
 
 function get_sspace(macro_data::Union{FloatMatrix, JMatrix{Float64}}, t0::Int64, optimal_hyperparams::FloatVector, model_args::Tuple, model_kwargs::NamedTuple, coordinates_params_rescaling::Vector{Vector{Int64}}, existing_sspace::KalmanSettings, existing_std_diff_data::FloatVector)
+    @infiltrate
     MessyTimeSeriesOptim.update_sspace_data!(existing_sspace, macro_data./existing_std_diff_data);
+    @infiltrate
     return existing_sspace;
 end
 
@@ -139,7 +143,9 @@ function get_macro_data_partitions(macro_vintage::AbstractDataFrame, equity_inde
     
     if include_factor_augmentation
 
+        @infiltrate
         sspace = get_sspace(macro_data, t0, optimal_hyperparams, model_args, model_kwargs, coordinates_params_rescaling, existing_sspace, existing_std_diff_data);
+        @infiltrate
         status = DynamicKalmanStatus();
 
         # Factors data
@@ -209,19 +215,19 @@ function estimate_and_validate(estimation_samples_target::FloatVector, estimatio
     
     # Generate `model` instance
     model_instance = model(; model_settings...);
-    @infiltrate
+    @infiltrate 
 
     # Estimation
     ScikitLearn.fit!(model_instance, permutedims(estimation_samples_predictors), estimation_samples_target); # in ScikitLearn all input predictors matrices are vertical - i.e., of shape (n_sample, n_feature)
-    @infiltrate
+    @infiltrate 
 
     # Validation sample forecasts
     validation_forecasts = ScikitLearn.predict(model_instance, permutedims(validation_samples_predictors)); # in ScikitLearn all input predictors matrices are vertical - i.e., of shape (n_sample, n_feature)
-    @infiltrate
+    @infiltrate 
 
     # Compute validation error
     validation_error = mean((validation_samples_target .- validation_forecasts).^2);
-    @infiltrate
+    @infiltrate 
 
     # Return output
     return model_instance, validation_forecasts, validation_error;
