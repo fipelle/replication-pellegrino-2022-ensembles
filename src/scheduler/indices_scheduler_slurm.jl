a154e368f6365e02f715144ca9936b1046af43ed
fipelle <6272230+fipelle@users.noreply.github.com>
2022-12-08 19:49:38 +0000

    Up-to-date slurm indices scheduler

--- src/scheduler/indices_scheduler_slurm.jl
+++ src/scheduler/indices_scheduler_slurm.jl
@@ -1,28 +1,25 @@
-function get_qsub_content(equity_index_id::Int64, regression_model::Int64, compute_ep_cycle::Bool, include_factor_augmentation::Bool, include_factor_transformations::Bool)
+function get_sbatch_content(equity_index_id::Int64, regression_model::Int64, compute_ep_cycle::Bool, include_factor_augmentation::Bool, include_factor_transformations::Bool)
     
     julia_log_folder_path = ifelse(compute_ep_cycle, "./BC_and_EP_output", "./BC_output");
-    qsub_log_output = "\$HOME/Documents/replication-pellegrino-2022-ensembles/src/fabian/logs/$(regression_model)/\$JOB_NAME.\$JOB_ID.output";
-    qsub_name = "m$(equity_index_id)_$(regression_model)_$(compute_ep_cycle)_$(include_factor_augmentation)_$(include_factor_transformations)";
-    qsub_command = "julia finance_forecasts.jl $(equity_index_id) $(regression_model) $(compute_ep_cycle) $(include_factor_augmentation) $(include_factor_transformations) $(julia_log_folder_path)"
+    sbatch_name = "m$(equity_index_id)_$(regression_model)_$(compute_ep_cycle)_$(include_factor_augmentation)_$(include_factor_transformations)";
+    sbatch_command = "julia finance_forecasts.jl $(equity_index_id) $(regression_model) $(compute_ep_cycle) $(include_factor_augmentation) $(include_factor_transformations) $(julia_log_folder_path)"
 
-    qsub_content = """
+    sbatch_content = """
     #!/bin/bash -login
-    #\$ -wd \$HOME/Documents/replication-pellegrino-2022-ensembles/src
-    #\$ -V
-    #\$ -j y
-    #\$ -o $(qsub_log_output)
-    #\$ -N $(qsub_name)
-    #\$ -M f.pellegrino1@lse.ac.uk
-    #\$ -m bea
-    #\$ -l h_rt=144:0:0
-    #\$ -l h_vmem=64G
-    #\$ -l h='(vnode01|vnode02|vnode03|vnode06|vnode08|vnode13|vnode16)'
-    #\$ -pe smp 1
+    #!/bin/bash
 
-    module load apps/julia/1.6.2
-    $(qsub_command)""";
+    #SBATCH --partition=xlarge
+    #SBATCH --nodes=1
+    #SBATCH --cpus-per-task 4
+    #SBATCH --job-name=$(sbatch_name)
+    #SBATCH --error=./logs/$(regression_model)/%x_%N_%j.err
+    #SBATCH --output=./logs/$(regression_model)/%x_%N_%j.out
+    #SBATCH --chdir="../"
+    
+    module add apps/julia/1.6.7
+    $(sbatch_command)""";
 
-    return qsub_content;
+    return sbatch_content;
 end
 
 # Loop over the equity indices
@@ -31,21 +28,21 @@ for equity_index_id=11:20
         for compute_ep_cycle=[false; true]
             for (include_factor_augmentation, include_factor_transformations) in [(false, false), (true, false), (true, true)]
 
-                # Get qsub content
-                qsub_content = get_qsub_content(equity_index_id, regression_model, compute_ep_cycle, include_factor_augmentation, include_factor_transformations);
+                # Get sbatch content
+                sbatch_content = get_sbatch_content(equity_index_id, regression_model, compute_ep_cycle, include_factor_augmentation, include_factor_transformations);
 
-                # Setup qsub
-                open("index.qsub", "w") do io
-                    write(io, qsub_content)
+                # Setup sbatch
+                open("index.sbatch", "w") do io
+                    write(io, sbatch_content)
                 end;
                 
-                # Save backup qsub
-                open("./logs/$(regression_model)/scheduler_equity_index_$(equity_index_id)_$(compute_ep_cycle)_$(include_factor_augmentation)_$(include_factor_transformations).qsub", "w") do io
-                    write(io, qsub_content)
+                # Save backup sbatch
+                open("./logs/$(regression_model)/scheduler_equity_index_$(equity_index_id)_$(compute_ep_cycle)_$(include_factor_augmentation)_$(include_factor_transformations).sbatch", "w") do io
+                    write(io, sbatch_content)
                 end;
 
-                # Run qsub
-                run(`qsub index.qsub`);
+                # Run sbatch
+                run(`sbatch index.sbatch`);
 
                 # Wait before starting the next iteration
                 sleep(2.5);
